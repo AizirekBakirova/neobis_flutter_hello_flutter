1db523d7a9d44220b671030211edf8ebbcb062b8
Aizirek Bakirova <aizirek.bakirova@gmail.com>
2024-05-02 05:39:45 +0600

    add

--- lib/components/todo_widget.dart
+++ lib/components/todo_widget.dart
@@ -1,45 +1,58 @@
 // ignore_for_file: sort_child_properties_last, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class ToDoWidget extends StatelessWidget {
   ToDoWidget(
       {super.key,
       required this.task,
       required this.taskCompleted,
-      required this.onChanged});
+      required this.onChanged,
+      required this.deleteFunction});
 
   final String task;
   final bool taskCompleted;
   Function(bool?)? onChanged;
+  Function(BuildContext)? deleteFunction;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(right: 25, left: 25, top: 25),
-      child: Container(
-        padding: EdgeInsets.all(25),
-        decoration: BoxDecoration(
-          color: Colors.purple[600],
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: Row(
-          children: [
-            Checkbox(
-              value: taskCompleted,
-              onChanged: onChanged,
-              activeColor: Colors.black,
-            ),
-            Text(
-              task,
-              style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.black,
-                  decoration: taskCompleted
-                      ? TextDecoration.lineThrough
-                      : TextDecoration.none),
-            ),
-          ],
+      child: Slidable(
+        endActionPane: ActionPane(motion: StretchMotion(), children: [
+          SlidableAction(
+            onPressed: deleteFunction,
+            icon: Icons.delete,
+            backgroundColor: Colors.red,
+            borderRadius: BorderRadius.circular(12),
+          )
+        ]),
+        child: Container(
+          padding: EdgeInsets.all(25),
+          decoration: BoxDecoration(
+            color: Colors.purple[600],
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Row(
+            children: [
+              Checkbox(
+                value: taskCompleted,
+                onChanged: onChanged,
+                activeColor: Colors.black,
+              ),
+              Text(
+                task,
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.black,
+                    decoration: taskCompleted
+                        ? TextDecoration.lineThrough
+                        : TextDecoration.none),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/features/presentation/pages/home_page.dart
+++ lib/features/presentation/pages/home_page.dart
@@ -47,6 +47,12 @@ class _HomePageState extends State<HomePage> {
         });
   }
 
+  void deleteTask(int index) {
+    setState(() {
+      toDoList.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -64,9 +70,13 @@ class _HomePageState extends State<HomePage> {
             itemCount: toDoList.length,
             itemBuilder: (context, index) {
               return ToDoWidget(
-                  task: toDoList[index][0],
-                  taskCompleted: toDoList[index][1],
-                  onChanged: (value) => checkBoxChanged(value, index));
+                task: toDoList[index][0],
+                taskCompleted: toDoList[index][1],
+                onChanged: (value) => checkBoxChanged(value, index),
+                deleteFunction: (context) {
+                  deleteTask;
+                },
+              );
             }));
   }
 }
